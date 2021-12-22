6a5b1bc7bc9e0767edd6ff76f4f7db7e6dd98dd8
Антонов Павел <antonov@enterego.ru>
2021-12-22 12:57:35 +0300

    Add functional

--- src/components/sections/centerSection/CenterSection.jsx
+++ src/components/sections/centerSection/CenterSection.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import s from "./CenterSection.module.css";
 
-export default function CenterSection(){
-    return(<div className={s.centerSection}/>)
+export default function CenterSection() {
+
+    function hideBlock(e) {
+        e.target.closest(`.${s.centerSection}`).classList.toggle(`${s.hideElement}`);
+        let nexElement = e.target.closest(`.${s.centerSection}`).nextSibling;
+        nexElement.style.flexGrow = 1;
+    }
+
+    return (<div className={s.centerSection}>
+        <div className={s.hideBlock} onClick={hideBlock}/>
+    </div>)
 }
--- src/components/sections/rightSection/RightSection.jsx
+++ src/components/sections/rightSection/RightSection.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import s from "./RightSection.module.css";
 
-export default function RightSection(){
-    return(<div className={s.rightSection}/>)
+export default function RightSection() {
+
+    function hideBlock(e) {
+        e.target.closest(`.${s.rightSection}`).classList.toggle(`${s.hideElement}`);
+    }
+
+    return (<div className={s.rightSection}>
+        <div className={s.hideBlock} onClick={hideBlock}/>
+    </div>)
 }
